eef1ed8bfe56ee19bc0936241afd41b622a016bb
Ananya Jindal <ajindal1145@gmail.com>
2025-09-05 16:28:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (15.85%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<int> st;
+    bool isValid(string s){
+        //using map+stack
+        unordered_map<char, char> mappy;
+        mappy[']']='[';
+        mappy['}']='{';
+        mappy[')']='(';
+        stack<char> st;
         for(int i=0;i<s.length();i++){
-            if(st.empty() && (s[i]=='}' || s[i]==')' ||s[i]==']')) return false;
-            if(s[i]=='{' || s[i]=='(' ||s[i]=='[') st.push(s[i]);
-            else if(s[i]=='}' && st.top()=='{') st.pop();
-            else if(s[i]==']' && st.top()=='[') st.pop();
-            else if(s[i]==')' && st.top()=='(') st.pop();
-            else return false;
+            if(mappy.find(s[i])!=mappy.end()){ //basically agar closing bracket hai
+                if(!st.empty() && st.top()==mappy[s[i]]){
+                    st.pop();
+                }
+                else return false;
+            }
+            else{
+                st.push(s[i]);
+            }
         }
-        if(!st.empty()) return false;
-        return true;
+        return st.empty();
+        // stack<char> st;
+        // for(int i=0;i<s.length();i++){
+        //     if(st.empty() && (s[i]=='}' || s[i]==')' ||s[i]==']')) return false;
+        //     if(s[i]=='{' || s[i]=='(' ||s[i]=='[') st.push(s[i]);
+        //     else if(s[i]=='}' && st.top()=='{') st.pop();
+        //     else if(s[i]==']' && st.top()=='[') st.pop();
+        //     else if(s[i]==')' && st.top()=='(') st.pop();
+        //     else return false;
+        // }
+        // if(!st.empty()) return false;
+        // return true;
     }
 };
